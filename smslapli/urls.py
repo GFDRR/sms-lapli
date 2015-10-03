0be69a3701399b1c4ed574ba17b5da2569648959
Jeffrey Johnson <ortelius@gmail.com>
2015-10-03 11:17:37 -0400

    Move public to / and rapidsms to /rapidsms

--- smslapli/urls.py
+++ smslapli/urls.py
@@ -8,9 +8,11 @@ from rapidsms.backends.kannel.views import KannelBackendView
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+
     # RapidSMS Donnees_de_base URLs
     url(r'^accounts/', include('rapidsms.urls.login_logout')),
-    url(r'^$', 'rapidsms.views.dashboard', name='rapidsms-dashboard'),
+    url(r'^rapidsms/$', 'rapidsms.views.dashboard', name='rapidsms-dashboard'),
+
     # RapidSMS contrib app URLs
     url(r'^httptester/', include('rapidsms.contrib.httptester.urls')),
     url(r'^messagelog/', include('rapidsms.contrib.messagelog.urls')),
@@ -22,10 +24,12 @@ urlpatterns = [
     url(r'^selectable/', include('selectable.urls')),
     url(r"^backend/kannel-usb0-smsc/$", KannelBackendView.as_view(backend_name="kannel-usb0-smsc")),
     url(r"^rapport/pluie","public.views.rpluie"),
-    url(r"^public", "public.views.acc"),
     url(r"^json_rap", "public.views.json_rap"),
     url(r"^json_graph", "public.views.json_graph"),
     url(r"^test", "public.views.imp"),
+
+    url(r"^", "public.views.acc"),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
